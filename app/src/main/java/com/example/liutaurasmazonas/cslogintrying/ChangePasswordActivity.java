dc29e82305b0a3f0a73384a887f68d4e38086b38
aksinias@hotmail.com <aksinias@hotmail.com>
2017-03-06 13:53:30 +0000

    Added Logout to almost every screen Improved Settings page

--- app/src/main/java/com/example/liutaurasmazonas/cslogintrying/ChangePasswordActivity.java
+++ app/src/main/java/com/example/liutaurasmazonas/cslogintrying/ChangePasswordActivity.java
@@ -2,12 +2,48 @@ package com.example.liutaurasmazonas.cslogintrying;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 
 public class ChangePasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Button button = (Button) findViewById(R.id.bSave);
+        ChangePasswordActivityButtonListener listener = new ChangePasswordActivityButtonListener();
+        button.setOnClickListener(listener);
         setContentView(R.layout.activity_change_password);
+    }
+
+    protected void onFinishInflate() {
+
+    }
+
+    //I create Logout feature here. Ksenia
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menuLogout:
+
+                /* We need to create user in order to clear data
+                LoginPage.getInstance(this).logout();
+
+                */
+
+
+                break;
+        }
+        return true;
+
     }
 }
---
+++ app/src/main/java/com/example/liutaurasmazonas/cslogintrying/ChangePasswordActivityButtonListener.java
@@ -0,0 +1,15 @@
+package com.example.liutaurasmazonas.cslogintrying;
+
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by User on 02/03/2017.
+ */
+
+public class ChangePasswordActivityButtonListener implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
