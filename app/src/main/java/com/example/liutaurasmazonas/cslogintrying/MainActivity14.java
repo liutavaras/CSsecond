a9131b6e553e275ee96127fc2db3ed87337deb92
dragoscostin <dragos.costin97@yahoo.com>
2017-03-24 13:00:51 +0000

    Uptick

--- app/src/main/java/com/example/liutaurasmazonas/cslogintrying/MainActivity14.java
+++ app/src/main/java/com/example/liutaurasmazonas/cslogintrying/MainActivity14.java
@@ -1,13 +1,27 @@
 package com.example.liutaurasmazonas.cslogintrying;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity14 extends AppCompatActivity {
+import com.example.liutaurasmazonas.cslogintrying.WebViewActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity14 extends Activity {
+    private Button button;
+    public void onCreate(Bundle savedInstanceState) {
+        final Context context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main14);
+        button = (Button) findViewById(R.id.activity_main14);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(context, WebViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
