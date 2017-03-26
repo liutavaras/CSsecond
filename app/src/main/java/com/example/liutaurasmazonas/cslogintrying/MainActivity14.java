c3f01514769acbfb1bf20460b96a7bf3991d02ea
dragoscostin <dragos.costin97@yahoo.com>
2017-03-26 10:52:28 +0100

    Uptick

--- app/src/main/java/com/example/liutaurasmazonas/cslogintrying/MainActivity14.java
+++ app/src/main/java/com/example/liutaurasmazonas/cslogintrying/MainActivity14.java
@@ -1,11 +1,7 @@
 package com.example.liutaurasmazonas.cslogintrying;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
+
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.support.v7.widget.ButtonBarLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -13,29 +9,15 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import com.example.liutaurasmazonas.cslogintrying.WebViewActivity;
 public class MainActivity14 extends AppCompatActivity {
     RecyclerView recyclerView;
     ImageButton button;
 
 
-public class MainActivity14 extends Activity {
-    private Button button;
-    public void onCreate(Bundle savedInstanceState) {
-        final Context context = this;
-        super.onCreate(savedInstanceState);
     @Override
     protected void onCreate (Bundle savedInstantState) {
         super.onCreate(savedInstantState);
         setContentView(R.layout.activity_main14);
-        button = (Button) findViewById(R.id.activity_main14);
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Intent intent = new Intent(context, WebViewActivity.class);
-                startActivity(intent);
-            }
-        });
 
         button= (ImageButton)findViewById(R.id.googleImgBtn);
         button.setTag(1);
@@ -46,7 +28,6 @@ public class MainActivity14 extends Activity {
         ReadRss readRss=new ReadRss(this, recyclerView);
         readRss.execute();
     }
-}
 
     View.OnClickListener imageButtonHandler = new View.OnClickListener() {
 
