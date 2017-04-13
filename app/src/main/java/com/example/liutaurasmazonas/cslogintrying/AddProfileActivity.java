8020fb9b9ffd451df42fd74c7b5daf0899118396
JoeHJoscelyne <joe.joscelyne@yahoo.com>
2017-04-13 12:58:14 +0100

    Merge branch 'master' of https://github.com/liutavaras/CSsecond
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/liutaurasmazonas/cslogintrying/AddProfileActivity.java
+++ app/src/main/java/com/example/liutaurasmazonas/cslogintrying/AddProfileActivity.java
@@ -5,9 +5,39 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class AddProfileActivity extends AppCompatActivity {
+
+    EditText etName;
+    EditText etCareer;
+    CheckBox cbTech;
+    CheckBox cbMedi;
+    CheckBox nfRenewableEnergy;
+    CheckBox nfGoogle;
+    CheckBox cfNovartis;
+    CheckBox nfTesla;
+    Button News_Filtering_Save;
+    CheckBox lrsFB;
+    CheckBox lrsAAPL;
+    CheckBox lrsYHOO;
+    CheckBox cbEURUSD;
+    CheckBox lrcuUSDRUB;
+    CheckBox lrcoSILVER;
+    CheckBox lrcoGOLD;
+    CheckBox lrcuGBPUSD;
+    CheckBox lriNSDQ;
+    CheckBox lriSP500;
+    Button Live_Rates_Filtering_Save;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +45,55 @@ public class AddProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+         etName = (EditText) findViewById(R.id.etName);
+         etCareer = (EditText) findViewById(R.id.etCareer);
+         cbTech = (CheckBox) findViewById(R.id.cbTech);
+         cbMedi = (CheckBox) findViewById(R.id.cbMedi);
+         nfRenewableEnergy = (CheckBox) findViewById(R.id.nfRenewableEnergy);
+        nfGoogle = (CheckBox) findViewById(R.id.nfGoogle);
+        cfNovartis = (CheckBox) findViewById(R.id.cfNovartis);
+        nfTesla = (CheckBox) findViewById(R.id.nfTesla);
+        News_Filtering_Save = (Button) findViewById(R.id.News_Filtering_Save);
+        lrsFB = (CheckBox) findViewById(R.id.lrsFB);
+        lrsAAPL = (CheckBox) findViewById(R.id.lrsAAPL);
+        lrsYHOO = (CheckBox) findViewById(R.id.lrsYHOO);
+        cbEURUSD = (CheckBox) findViewById(R.id.cbEURUSD);
+        lrcuUSDRUB = (CheckBox) findViewById(R.id.lrcuUSDRUB);
+        lrcoSILVER = (CheckBox) findViewById(R.id.lrcoSILVER);
+        lrcoGOLD = (CheckBox) findViewById(R.id.lrcoGOLD);
+        lrcuGBPUSD = (CheckBox) findViewById(R.id.lrcuGBPUSD);
+        lriNSDQ = (CheckBox) findViewById(R.id.lriNSDQ);
+        lriSP500 = (CheckBox) findViewById(R.id.lriSP500);
+        Live_Rates_Filtering_Save = (Button) findViewById(R.id.bSave);
+
+
+        News_Filtering_Save.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                News_Filtering_Save();
+
+            }
+        });
+
+
+
+
+    }
+
+    private void News_Filtering_Save(){
+        String name = etName.getText().toString().trim();
+        String career = etCareer.getText().toString().trim();
+        String techcb = cbTech.getText().toString();
+
+        if(!TextUtils.isEmpty(name)){
+
+
+        }else{
+            Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show();
+
+        }
+
 
     }
 }
