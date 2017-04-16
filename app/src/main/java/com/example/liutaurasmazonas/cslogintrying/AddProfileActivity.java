c3ae7a329280e4a6b390fee53260f07141de4516
Liutauras Mazonas <liutavaras@gmail.com>
2017-04-17 00:19:40 +0100

    Merge branch 'master' of https://github.com/liutavaras/CSsecond
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/liutaurasmazonas/cslogintrying/AddProfileActivity.java
+++ app/src/main/java/com/example/liutaurasmazonas/cslogintrying/AddProfileActivity.java
@@ -1,27 +1,17 @@
 package com.example.liutaurasmazonas.cslogintrying;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -29,9 +19,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
-import static com.example.liutaurasmazonas.cslogintrying.R.id.Sentbutton;
 import static com.example.liutaurasmazonas.cslogintrying.R.id.addAProfile;
-import static com.example.liutaurasmazonas.cslogintrying.R.id.spinner;
 
 public class AddProfileActivity extends AppCompatActivity {
 
@@ -54,11 +42,11 @@ public class AddProfileActivity extends AppCompatActivity {
     CheckBox lriNSDQ;
     CheckBox lriSP500;
     Button bSave;
-    Spinner spinner5;
-    Button galleryspinner;
+
 
 
     DatabaseReference databaseClients;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,7 +76,6 @@ public class AddProfileActivity extends AppCompatActivity {
         lriNSDQ = (CheckBox) findViewById(R.id.lriNSDQ);
         lriSP500 = (CheckBox) findViewById(R.id.lriSP500);
         bSave = (Button) findViewById(R.id.bSave);
-        spinner5 = (Spinner) findViewById(R.id.spinner5);
 
 
         ImageButton bSettingsBlack = (ImageButton) findViewById(R.id.ibSettingsBlack);
@@ -127,61 +114,7 @@ public class AddProfileActivity extends AppCompatActivity {
 
 
 
-
-
-        int[] array = getResources().getIntArray(R.array.images_array);
-        String [] objects = new String[array.length];
-        for(int i = 0; i != array.length; i++){
-            objects[i] = "" + array[i];
-        }
-
-        spinner5.setAdapter(new MyAdapter(AddProfileActivity.this, R.id.spinner5, objects));
-
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_profile, menu);
-        return true;
-    }
-
-    class MyAdapter extends ArrayAdapter<String>{
-
-        String[] objects;
-        public MyAdapter(Context context, int textViewResourceId,   String[] objects) {
-            super(context, textViewResourceId, objects);
-            this.objects = objects;
-        }
-
-        @Override
-        public View getDropDownView(int position, View convertView,ViewGroup parent) {
-            return getCustomView(position, convertView, parent);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            return getCustomView(position, convertView, parent);
-        }
-
-        public View getCustomView(int position, View convertView, ViewGroup parent) {
-
-            LayoutInflater inflater=getLayoutInflater();
-            View row=inflater.inflate(R.layout.row, parent, false);
-
-
-            ImageView icon = (ImageView) row.findViewById(R.id.icon);
-            TypedArray imgs = getResources().obtainTypedArray(R.array.images_array);
-            icon.setImageResource(imgs.getResourceId(position, -1));
-
-            return row;
-        }
-    }
-
-
-
-
-
 
     private void bSave(){
         String name = etName.getText().toString().trim();
@@ -202,14 +135,14 @@ public class AddProfileActivity extends AppCompatActivity {
         Boolean gbpusdCU = lrcuGBPUSD.isChecked();
         Boolean nsdqI = lriNSDQ.isChecked();
         Boolean sp500I = lriSP500.isChecked();
-        String valueOfSelectedPos = spinner5.getSelectedItem().toString().trim();
+
 
         if(!TextUtils.isEmpty(name)){
           String id =  databaseClients.push().getKey();
 
             Clients clients = new Clients(id, name, career, techCB, mediCB, renewableEnergyNF, googleNF,
                     novartisNF, teslaNF, fbLRS, applLRS, yhooLRS, eurusdCB, usdrubCU, silverCO,
-                    goldCO, gbpusdCU, nsdqI, sp500I, valueOfSelectedPos );
+                    goldCO, gbpusdCU, nsdqI, sp500I );
 
             databaseClients.child(id).setValue(clients);
 
@@ -224,8 +157,7 @@ public class AddProfileActivity extends AppCompatActivity {
 
         }
 
-    }
 
     }
 
-
+}
