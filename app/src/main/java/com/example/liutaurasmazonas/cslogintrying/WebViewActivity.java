f4ac2253a3f389856af8c3dca801f15d692608a6
JoeHJoscelyne <joe.joscelyne@yahoo.com>
2017-04-20 00:44:05 +0100

    Merge branch 'master' of https://github.com/liutavaras/CSsecond
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/liutaurasmazonas/cslogintrying/AddProfileActivity.java
    #	app/src/main/java/com/example/liutaurasmazonas/cslogintrying/Clients.java

--- app/src/main/java/com/example/liutaurasmazonas/cslogintrying/WebViewActivity.java
+++ app/src/main/java/com/example/liutaurasmazonas/cslogintrying/WebViewActivity.java
@@ -1,8 +1,12 @@
 package com.example.liutaurasmazonas.cslogintrying;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.ImageButton;
+
 public class WebViewActivity extends Activity {
     private WebView webView;
     public void onCreate(Bundle savedInstanceState) {
@@ -11,5 +15,41 @@ public class WebViewActivity extends Activity {
         webView = (WebView) findViewById(R.id.webView1);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("http://www.tradingeconomics.com/calendar");
+
+
+
+        ImageButton bSettingsBlack = (ImageButton) findViewById(R.id.ibSettingsBlack);
+        ImageButton bEconCalBlack = (ImageButton) findViewById(R.id.ibEconCalBlack);
+        ImageButton bLiveRates = (ImageButton) findViewById(R.id.ibLiveRates);
+        ImageButton bNewsBlack = (ImageButton) findViewById(R.id.ibNewsBlack);
+        ImageButton addAProfile = (ImageButton) findViewById(R.id.addAProfile);
+
+        bSettingsBlack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(WebViewActivity.this, SettingsActivity.class));
+            }
+        });
+        bEconCalBlack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(WebViewActivity.this, WebViewActivity.class));
+            }
+        });
+        bLiveRates.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(WebViewActivity.this, sortingrates.class));
+            }
+        });
+        bNewsBlack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(WebViewActivity.this, HomePageNews.class));
+            }
+        });
+        addAProfile.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(WebViewActivity.this, AddProfileActivity.class));
+            }
+        });
+
+
     }
 }
